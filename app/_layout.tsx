30c4c3c3ae7d430bad50f88a388923b32a53e93f
Matheus Cassioli <matheuscassioli@hotmail.com>
2025-07-03 23:26:36 -0300

    hotfix(aplicacao) : login corrigido + arquivos inutilizados removidos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,69 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export const Login = () => {
+  return (
+    <View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputContainerLabel}>usuario</Text>
+        <TextInput style={styles.input} />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputContainerLabel}>senha</Text>
+        <TextInput style={styles.input} />
+      </View>
+
+      <Pressable
+        onPress={() => alert("entrar")}
+        style={{ backgroundColor: "purple" }}
+      >
+        <Text>Entrar</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <Login />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    backgroundColor: "lightblue",
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  container: {
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1,
+  },
+  text: {
+    fontSize: 18,
+    color: "red",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputContainerLabel: {
+    padding: 10,
+    marginBottom: -18,
+  },
+  inputContainer: {
+    backgroundColor: "yellow",
+  },
+});
